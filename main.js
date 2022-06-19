0405f1f6e179f9fdda9e7750f9a68cca59d8debc
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-06-19 14:19:14 +1000

    added JS code and added logo resizing

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener('resize', function() {
+    var logo_full = document.getElementById("logo_full")
+    var logo_small = document.getElementById("logo_small")
+    var viewport_width = window.innerWidth;
+    if (viewport_width < 625) {
+        logo_full.className = "invisible";
+        logo_small.className = "";
+    } else {
+        logo_small.className = "invisible";
+        logo_full.className = "";
+    }
+});
