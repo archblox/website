41b4cf91703238d620266f7c57d4d8a45db0d34b
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-06-20 12:47:29 +1000

    made link to published site more noticable, and removed unnessasary code from index.js as that only loads on 1 page.

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-window.addEventListener('resize', function() {
-    var logo_full = document.getElementById("logo_full")
-    var logo_small = document.getElementById("logo_small")
-    var viewport_width = window.innerWidth;
-    if (viewport_width < 900) {
-        logo_full.className = "invisible";
-        logo_small.className = "";
-    } else {
-        logo_small.className = "invisible";
-        logo_full.className = "";
-    }
-});
 window.onload = function() {
-    var alert = document.getElementById("alert")
-    var alerttext = document.getElementById("alerttext")
     var morblox = document.getElementById("morblox")
     if (window.location.hostname == "") {   
-        alerttext.innerHTML = "You are using this locally.";
-        alert.className = "";
         morblox.className = "";
     } else if (window.location.hostname == "thomasluigi07.github.io") {
-        alerttext.innerHTML = "You are using this on the github pages site.";
-        alert.className = "";
         morblox.className = "";
     } else if (window.location.hostname == "MORBLOX.com") {
-            alerttext.innerHTML = "You are using MORBLOX.com";
-            alert.className = "";
             morblox.className = "";
     } else if (window.location.hostname == "morblox.us") {
-        alerttext.innerHTML = "You are using morblox.us";
-        alert.className = "";
         morblox.className = "invisible";
     } else {
-        alerttext.innerHTML = `You are using an unknown site (${window.location.hostname})`;
-        alert.className = "";
         morblox.className = "";
     }
 };
