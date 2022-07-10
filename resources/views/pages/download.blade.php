46d5084c34d225dd91361fe7c71da87f275875fa
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-11 01:10:24 +1000

    Version 1.7 for launcher

--- resources/views/pages/download.blade.php
+++ resources/views/pages/download.blade.php
@@ -32,10 +32,10 @@
         <strong>Requires Windows 7 or newer and Microsoft .net 3.1.</strong>
         <p>ARCHBLOX will be installed to %LOCALAPPDATA%\Archblx\Versions</p>
         <br>
-        <p>ARCHBLOX Bootstrapper v1.1 (Download this first)</p>
-        <p><a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-32bit.exe') }}">32 Bit</a> <a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-64bit.exe') }}">64 Bit</a></p>
-        <br>
-        <p>ARCHBLOX Launcher v1.6 (Use after downloading and using the ARCHBLOX Bootstrapper)</p>
+        <!--<p>ARCHBLOX Bootstrapper v1.1 (Download this first)</p>-->
+        <!--<p><a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-32bit.exe') }}">32 Bit</a> <a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-64bit.exe') }}">64 Bit</a></p>-->
+        <!--<br>-->
+        <p>ARCHBLOX Launcher v1.7</p>
         <p><a href="{{ asset('launcher/ARCHBLOXLauncher-32bit.exe') }}">32 Bit</a> <a href="{{ asset('launcher/ARCHBLOXLauncher-64bit.exe') }}">64 Bit</a></p>
         <br>
         <p>If you get a popup saying "Windows protected your PC", press "More info" and then press "Run anyway".</p>
