90ab2030dc448907bf9ed45ded2aa62c6565757c
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-12 20:33:02 +1000

    2.3

--- resources/views/pages/download.blade.php
+++ resources/views/pages/download.blade.php
@@ -35,7 +35,7 @@
         <!--<p>ARCHBLOX Bootstrapper v1.1 (Download this first)</p>-->
         <!--<p><a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-32bit.exe') }}">32 Bit</a> <a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-64bit.exe') }}">64 Bit</a></p>-->
         <!--<br>-->
-        <p>ARCHBLOX Launcher v2.2</p>
+        <p>ARCHBLOX Launcher v2.3</p>
         <p><a href="{{ asset('launcher/ARCHBLOXLauncher-32bit.exe') }}">32 Bit</a> <a href="{{ asset('launcher/ARCHBLOXLauncher-64bit.exe') }}">64 Bit</a></p>
         <br>
         <p>If you get a popup saying "Windows protected your PC", press "More info" and then press "Run anyway".</p>
