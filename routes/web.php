0508692d7278af9fc27ea155e03af8d3ab03e593
Conkley <conkley@yahoo.com>
2022-07-12 12:36:11 +1000

    route change

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,7 @@ Route::group(['middleware' => 'AdminCheck'], function() {
 Route::get('/game/studio.ashx', [App\Http\Controllers\ClientController::class, 'studio'])->name('studio');
 Route::get('/game/visit.ashx', [App\Http\Controllers\ClientController::class, 'visit'])->name('visit');
 Route::get('/game/ChatFilter.ashx', [App\Http\Controllers\ClientController::class, 'chatfilter'])->name('chatfilter');
-Route::get('/game/join.aspx', [App\Http\Controllers\ClientController::class, 'join'])->name('join');
+Route::get('/game/join.ashx', [App\Http\Controllers\ClientController::class, 'join'])->name('join');
 Route::get('/ide/welcome', [App\Http\Controllers\ClientController::class, 'ide'])->name('ide');
 Route::get('/IDE/ClientToolbox.aspx', [App\Http\Controllers\ClientController::class, 'toolbox'])->name('toolbox');
 Route::get('/UploadMedia/PostImage.aspx', [App\Http\Controllers\ClientController::class, 'postimage'])->name('postimage');
