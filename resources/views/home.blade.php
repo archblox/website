3814a30642541241a03f25c690593635ab549f34
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-13 01:42:46 +1000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
             <h2>My Feed</h2>
             <br>
             <input id="FeedBox" type="text" placeholder="Say something...">
-            <button class="greybutton" id="FeedButton">Post it!</button>
+            <button style="width: 100%;" class="greybutton" id="FeedButton">Post it!</button>
             <br>
             <div id="FeedContainer">
                 <div class="FeedContainerBox" id="FeedContainerBox1">
@@ -26,65 +26,9 @@
                                 width="60px" height="100%"></a>
                     </div>
                     <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                        <a href="#" id="FeedContainerBox1Username">skeuomorphism hater</a>
-                        <p id="FeedContainerBox1Text">"noooooo"</p>
-                        <p id="FeedContainerBox1Timestamp">June 21, 2022 04:42 AM</p>
-                    </div>
-                </div>
-                <div class="FeedContainerBox" id="FeedContainerBox1">
-                    <div class="FeedContainerBoxImageContainer" id="FeedContainerBox1ImageContainer">
-                        <a href="#"><img alt="Profile Image" src="{{ asset('img/reviewpending.png') }}"
-                                width="60px" height="100%"></a>
-                    </div>
-                    <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                        <a href="#" id="FeedContainerBox1Username">Thomas</a>
-                        <p id="FeedContainerBox1Text">"grrr im banning you"</p>
-                        <p id="FeedContainerBox1Timestamp">June 21, 2022 04:29 AM</p>
-                    </div>
-                </div>
-                <div class="FeedContainerBox" id="FeedContainerBox1">
-                    <div class="FeedContainerBoxImageContainer" id="FeedContainerBox1ImageContainer">
-                        <a href="#"><img alt="Profile Image" src="{{ asset('img/reviewpending.png') }}"
-                                width="60px" height="100%"></a>
-                    </div>
-                    <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                        <a href="#" id="FeedContainerBox1Username">skeuomorphism hater</a>
-                        <p id="FeedContainerBox1Text">"i dont"</p>
-                        <p id="FeedContainerBox1Timestamp">June 21, 2022 04:28 AM</p>
-                    </div>
-                </div>
-                <div class="FeedContainerBox" id="FeedContainerBox1">
-                    <div class="FeedContainerBoxImageContainer" id="FeedContainerBox1ImageContainer">
-                        <a href="#"><img alt="Profile Image" src="{{ asset('img/reviewpending.png') }}"
-                                width="60px" height="100%"></a>
-                    </div>
-                    <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                        <a href="#" id="FeedContainerBox1Username">Thomas</a>
-                        <p id="FeedContainerBox1Text">"Who likes my epic new CSS buttons? They're way more interesting than
-                            the default buttons."</p>
-                        <p id="FeedContainerBox1Timestamp">June 21, 2022 04:27 AM</p>
-                    </div>
-                </div>
-                <div class="FeedContainerBox" id="FeedContainerBox1">
-                    <div class="FeedContainerBoxImageContainer" id="FeedContainerBox1ImageContainer">
-                        <a href="#"><img alt="Profile Image" src="{{ asset('img/reviewpending.png') }}"
-                                width="60px" height="100%"></a>
-                    </div>
-                    <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                        <a href="#" id="FeedContainerBox1Username">the night guard from five nights at freddy</a>
-                        <p id="FeedContainerBox1Text">"OH LAWD ITS 3 AM! FREDDY FAZBER IS AT MY DOOR PLEASE HELP!!!!"</p>
-                        <p id="FeedContainerBox1Timestamp">June 21, 2022 03:19 AM</p>
-                    </div>
-                </div>
-                <div class="FeedContainerBox" id="FeedContainerBox1">
-                    <div class="FeedContainerBoxImageContainer" id="FeedContainerBox1ImageContainer">
-                        <a href="#"><img alt="Profile Image" src="{{ asset('img/reviewpending.png') }}"
-                                width="60px" height="100%"></a>
-                    </div>
-                    <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                        <a href="#" id="FeedContainerBox1Username">Thomas</a>
-                        <p id="FeedContainerBox1Text">"Why am I staying up so late making this."</p>
-                        <p id="FeedContainerBox1Timestamp">June 21, 2022 12:30 AM</p>
+                        <a href="#" id="FeedContainerBox1Username">Placeholder</a>
+                        <p id="FeedContainerBox1Text">"This is a placeholder for future My Feed posts."</p>
+                        <p id="FeedContainerBox1Timestamp">July 13, 2022 01:42 AM</p>
                     </div>
                 </div>
             </div>
