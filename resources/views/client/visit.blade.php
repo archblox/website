c7661d0dbe2b5e4e73765eb5d52177f38448135e
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-18 02:16:07 +1000

    Update visit.blade.php

--- resources/views/client/visit.blade.php
+++ resources/views/client/visit.blade.php
@@ -3,12 +3,6 @@
 header("content-type:text/plain");
 ob_start();
 
-if (Auth::check()) {
-	$username = addslashes(Auth::user()->name);
-} else {
-	$username = "Player";
-}
-
 @endphp
 
 -- Prepended to Edit.lua and Visit.lua and Studio.lua and PlaySolo.lua--
@@ -103,10 +97,10 @@ function doVisit()
 	message.Text = "Creating Player"
 	if false then
 		player = game:GetService("Players"):CreateLocalPlayer(0)
+		player.Name = [====[Guest 4708]====]
 	else
 		player = game:GetService("Players"):CreateLocalPlayer(0)
 	end
-	player.Name = "<?php echo $username; ?>"
 	player.CharacterAppearance = "http://www.morblox.us/Asset/CharacterFetch.ashx?userId=1&placeId=0"
 	local propExists, canAutoLoadChar = false
 	propExists = pcall(function()  canAutoLoadChar = game.Players.CharacterAutoLoads end)
