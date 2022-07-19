4a9d3e3e7d6e3eb64e131c3d7ec7ade24bb5c57d
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-19 20:49:13 +1000

    Update download.blade.php

--- resources/views/pages/download.blade.php
+++ resources/views/pages/download.blade.php
@@ -17,9 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/main.js') }}"></script>
 </head>
-
 <body>
     <br>
     <div id="logo_signup">
