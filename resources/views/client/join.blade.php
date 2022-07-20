8a29a9715ed1768095b4a8427e69bb202adeade9
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-20 22:27:46 +1000

    Added back id 400 error

--- resources/views/client/join.blade.php
+++ resources/views/client/join.blade.php
@@ -13,8 +13,8 @@ if (Auth::check()) {
 	$id = addslashes(Auth::id());
 } else {
 	$randName = mt_rand(1, 9999);
-	$username = "ARCHBLOXUser " . $randName;
-	$id = $randName;
+	$username = "Guest " . $randName;
+	$id = 0;
 }
 $ip = addslashes($_GET["ip"]);
 $port = addslashes($_GET["port"]);
