97239f099a468b722108915c815af23b3355ca60
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-23 01:20:03 +1000

    Update blog.blade.php

--- resources/views/misc/blog.blade.php
+++ resources/views/misc/blog.blade.php
@@ -21,7 +21,7 @@
 <body style="    height: 100%;
     margin: 0;
     width: 100%;">
-    <iframe src="https://archblox.blogspot.com" width="100%" height="100%"></iframe>
+    <iframe src="https://archblox.blogspot.com" width="100%" height="100%" style="border:none;"></iframe>
 </body>
 
 </html>
