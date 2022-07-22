43c5f089581ee9ce36bea341b438f2552709d509
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-23 00:26:33 +1000

    fixed link issue

--- resources/views/pages/incomplete.blade.php
+++ resources/views/pages/incomplete.blade.php
@@ -13,5 +13,5 @@
         <h1 id="usernameframe">Incomplete Page</h1>
         <br>
         <p>This page is not done yet. This page will be made soon, so please be patient.</p>
-        <p>Looking for the download links for the client/studio? <a href="{{ route('download')">Click here</a>.</p>
+        <p>Looking for the download links for the client/studio? <a href="{{ route('download') }}">Click here</a>.</p>
     @endsection
