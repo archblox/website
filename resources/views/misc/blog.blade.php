174e91da854132dcaacbe07fa986b7d8490874ca
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-23 04:14:09 +1000

    Update blog.blade.php

--- resources/views/misc/blog.blade.php
+++ resources/views/misc/blog.blade.php
@@ -1,18 +1,25 @@
 @extends('layouts.app')
-    @section('title')
-        <title>Blog - {{ env('APP_NAME') }}</title>
-    @endsection
-    @section('titlediscord')
-        <meta content="Blog - {{ env('APP_NAME') }}" property="og:title" />
-    @endsection
-    @section('descdiscord')
-        <meta content="ARCHBLOX is a work in progress revival." property="og:description" />
-    @endsection
-    @section('extras')
-    <base target="_parent">
-    @endsection
-<body style="height: 100%; margin: 0; width: 100%; overflow: hidden;">
-    @section('custom_content')
-        <iframe src="https://archblox.blogspot.com" title="ARCHBLOX Blog" width="100%" height="100%" style="border:none;"></iframe>
-    @endsection
-</body>
+@section('title')
+<title>Blog - {{ env('APP_NAME') }}</title>
+@endsection
+@section('titlediscord')
+<meta content="Blog - {{ env('APP_NAME') }}" property="og:title" />
+@endsection
+@section('descdiscord')
+<meta content="ARCHBLOX is a work in progress revival." property="og:description" />
+@endsection
+@section('extras')
+<base target="_parent">
+<style>
+body {
+    height: 100%;
+    margin: 0;
+    width: 100%;
+    overflow: hidden;
+}
+</style>
+@endsection
+@section('custom_content')
+<iframe src="https://archblox.blogspot.com" title="ARCHBLOX Blog" width="100%" height="100%"
+    style="border:none;"></iframe>
+@endsection
