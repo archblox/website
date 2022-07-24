01ad9791757dc818c948fadf590acdf0af3112c0
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-07-24 21:13:32 +1000

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
                     <input id="FeedBox" type="text" name="status" placeholder="Say something..." style="width: 80%;"
                         value="{{ old('status') }}">
                     <button style="width: 20%;height: 28px;margin-left: 10px;" class="greybutton" id="FeedButton"
-                        type="submit" alt="Post Button" onClick="this.form.submit();this.disabled=true;this.innerText='Posting…';">Post
+                        type="submit" alt="Post it!, Button" onClick="this.form.submit();this.disabled=true;this.innerText='Posting…';">Post
                         it!</button>
                 </p>
                 @if ($errors->any())
@@ -37,18 +37,18 @@
                 @foreach ($data['posts'] as $post)
                     <div class="FeedContainerBox" id="FeedContainerBox1">
                         <div class="FeedContainerBoxImageContainer" id="FeedContainerBox1ImageContainer">
-                            <a href="{{ route('profile', $post->user->id) }}"><img alt="Profile Image"
+                            <a href="{{ route('profile', $post->user->id) }}"><img alt="A image of {{ $post->user->name }}"
                                     src="{{ asset('img/defaultrender.png') }}" width="60px" height="100%"></a>
                         </div>
                         <div class="FeedContainerBoxTextContainer" id="FeedContainerBox1TextContainer">
-                            <a href="{{ route('profile', $post->user->id) }}"
+                            <a alt="{{ $post->user->name }} says" href="{{ route('profile', $post->user->id) }}"
                                 id="FeedContainerBox1Username">{{ $post->user->name }}</a>
                             <p id="FeedContainerBox1Text" style="word-wrap:break-word;max-width:400px">
                                 "{{ $post->status }}"</p>
                             @if (!Auth::user()->settings->time_preference_24hr)
-                                <p id="FeedContainerBox1Timestamp">{{ $post->created_at->format('F d, Y h:i A') }}</p>
+                                <p id="FeedContainerBox1Timestamp" alt="">{{ $post->created_at->format('F d, Y h:i A') }}</p>
                             @else
-                                <p id="FeedContainerBox1Timestamp">{{ $post->created_at->format('F d, Y H:i') }}</p>
+                                <p id="FeedContainerBox1Timestamp" alt="">{{ $post->created_at->format('F d, Y H:i') }}</p>
                             @endif
                         </div>
                     </div>
@@ -63,7 +63,7 @@
             <div class="content_special" style="justify-content: center;">
                 <h2>Friends ({{ Auth::user()->getFriendsCount() }})</h2>
                 @if (Auth::user()->getFriendsCount() > 0)
-                    <a href="{{ route('friends') }}" style="margin-left: 5px"> <button class="bluebutton"
+                    <a alt="View All, button" href="{{ route('friends') }}" style="margin-left: 5px"> <button class="bluebutton"
                             style="margin-top: 5px">View
                             All</button></a>
                 @endif
