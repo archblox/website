2ed09a13036a66d67f2ed6c389fff2233034d359
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-03 09:34:55 +1000

    Update notapproved.blade.php

--- resources/views/notapproved.blade.php
+++ resources/views/notapproved.blade.php
@@ -27,13 +27,16 @@
         <p id="morbin">We're Still Morbin'</p>
     </div>
     <div class="content_signup">
-        <h1>Account Deleted</h1> <!-- Ban Type Goes here (Warning, [day number] Day Ban, Account Deleted) -->
-        <p>01/01/1970 12:00 AM</p> <!-- Timestamp -->
+        <h1>Account Deleted</h1> <!-- Ban Type Goes here (Warning, [day number] Day Ban, Account Deleted, Account Disabled) -->
+        <p><strong>Action taken on:</strong> 01/01/1970 12:00 AM</p> <!-- Timestamp -->
+        <p><strong>Reason:</strong> liking the iphone 6</p> <!-- Reason -->
         <p><strong>Moderator Note:</strong> i like men</p> <!-- Moderator Note -->
         <p>If you would like to appeal, please join our discord server.</p> <!-- If not banned from the server -->
         <!-- <p>You are unable to appeal your ban.</p> --> <!-- If banned from the server -->
-        <button class="greybutton">Re-Activate my account</button> <!-- Show this button once the ban time period is over or it is a warn -->
-        <button class="greybutton">Log out</button> <!-- Show this button only if the account is terminated, or is currently in the ban time period.  -->
+        <!-- <p>To re-enable your account, click the button below.</p> --> <!-- If account is disabled only -->
+        <p>Your account will be fully deleted in 3 months.</p> <!-- If account is disabled/terminated only, show time remaining. -->
+        <button class="greybutton">Re-Activate my account</button> <!-- Show this button once the ban time period is over, the account is disabled or it is a warn -->
+        <button class="redbutton">Log out</button> <!-- Show this button only if the account is terminated, or is currently in the ban time period.  -->
     </div>
     <div id="footer_signup">
         <p>ARCHBLOX is not affiliated with Roblox Corp, Lego, Sony, SEGA, Microsoft, Nintendo or any other company. We're still Morbin'!</p>
