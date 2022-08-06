b1ad7ec3f9fe057af030e7bfba490092d447b694
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-07 06:10:56 +1000

    Update maintenance.blade.php

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -16,9 +16,9 @@
 <p style="color: #ffffff">Impaitient? <a href="https://discord.gg/nudzQ7hkWY">Join our Discord Server</a> for status updates.</p>
 @if (!Auth::guest() && Auth::user()->isAdmin())
 <br>
-<br>
 <p>Since you are an admin, you may access the main site during maintenance.</p>
 <strong>Note that some features may be broken during this period.</strong>
+<br>
 <a href="{{ route('admin_index') }}"><button class="bluebutton">Open Admin Panel</button></a>
 <a href="{{ route('home') }}"><button class="greybutton">Open Main Site</button></a>
 @endif
