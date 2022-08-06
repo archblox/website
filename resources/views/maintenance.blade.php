f49faf9f89ca0f6f2c3f19ea0473a8a690dfe210
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-07 06:05:56 +1000

    Update maintenance.blade.php

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -6,12 +6,12 @@
 <meta content="{{env('APP_NAME')}}" property="og:title" />
 @endsection
 @section('descdiscord')
-<meta content="ARCHBLOX" property="og:description" />
+<meta content="Currently under maintenance." property="og:description" />
 @endsection
 
 @section('content')
 <h1 style="color: #ffffff">Oh Noes! ARCHBLOX is currently under maintenance.</h1>
-<p style="color: #ffffff">ARCHBLOX will be back as soon as we can fix it.</p>
+<p style="color: #ffffff">ARCHBLOX will be back soon, so please be patient.</p>
 <br>
 <p style="color: #ffffff">Impaitient? <a href="https://discord.gg/nudzQ7hkWY">Join our Discord Server</a> for status
     updates.</p>
