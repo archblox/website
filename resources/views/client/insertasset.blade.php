07f70e65d0933cd83bdcc20047d7008722209b43
Conkley <conkley@yahoo.com>
2022-08-07 16:38:23 +1000

    stamper fixes

--- resources/views/client/insertasset.blade.php
+++ resources/views/client/insertasset.blade.php
@@ -3,7 +3,7 @@
     $nsets = (int)$_GET["nsets"];
     $type = (string)$_GET["type"];
     $userid = (int)$_GET["userid"];
-    //$sid = (int)$_GET["sid"];
+    $sid = (int)$_GET["sid"];
     //http://roblox.com/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=11744447
     if ($nsets == 20 && $type == "user")
     {
@@ -14,10 +14,10 @@
         //header("location:" . "http://morblox.us/stamper/base/{$userid}.xml");
     }
     //http://www.morblox.us/stamper/inseassetcollect.php?sid=433603
-    /*if ($sid != 0 && $type == "0")
+    if ($sid != 0 && $type == "0")
     {
         header("content-type:text/xml");
         $file = "http://morblox.us/stamper/sid/{$sid}.xml";
         readfile($file);
-    }*/
+    }
 @endphp
