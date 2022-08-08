d58a2da70856f2cbdd1b438b6307abe213be0abf
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-09 05:15:22 +1000

    Update insertasset.blade.php

--- resources/views/client/insertasset.blade.php
+++ resources/views/client/insertasset.blade.php
@@ -1,14 +1,30 @@
 @php    
 	//header("location:" . $file);
-    $nsets = (int)$_GET["nsets"];
-    $type = (string)$_GET["type"];
-    $userid = (int)$_GET["userid"];
-    $sid = (int)$_GET["sid"];
+    if(isset($_GET['nswts'])){
+        $nsets = (int)$_GET["nsets"];
+    } else {
+        $nsets = 0;
+    }
+    if(isset($_GET['type'])){
+        $type = (string)$_GET["type"];
+    } else {
+        $type = "";
+    }
+    if(isset($_GET['userid'])){
+        $userid = (int)$_GET["userid"];
+    } else {
+        $userid = 0;
+    }
+    if(isset($_GET['sid'])){
+        $sid = (int)$_GET["sid"];
+    } else {
+        $sid = 0;
+    }
     if ($nsets == 20 && $type == "user") {
         header("content-type:text/xml");
         $file = "http://morblox.us/stamper/base/{$userid}.xml";
         readfile($file);
-    } else if ($sid != 0 || !empty($sid) || $type == "0") {
+    } else if ($sid != 0 || $type == "0") {
         header("content-type:text/xml");
         $file = "http://morblox.us/stamper/sid/{$sid}.xml";
         readfile($file);
