2baa988d47166181bc5422a212a88504ecbc8f87
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-09 05:04:29 +1000

    Update insertasset.blade.php

--- resources/views/client/insertasset.blade.php
+++ resources/views/client/insertasset.blade.php
@@ -5,15 +5,13 @@
     $userid = (int)$_GET["userid"];
     $sid = (int)$_GET["sid"];
     //http://roblox.com/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=11744447
-    if ($nsets == 20 && $type == "user")
-    {
+    if ($nsets == 20 && $type == "user") {
         header("content-type:text/xml");
         $file = "http://morblox.us/stamper/base/{$userid}.xml";
         readfile($file);
         //
         //header("location:" . "http://morblox.us/stamper/base/{$userid}.xml");
-    } elseif ($sid != 0 && $type == "0")
-    {
+    } else if ($sid != 0 && $type == "0") {
         header("content-type:text/xml");
         $file = "http://morblox.us/stamper/sid/{$sid}.xml";
         readfile($file);
