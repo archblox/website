f9e5673db35ba79a1226a5ae6fd178e4a17c4366
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-08 22:17:45 +1000

    fixxed loggedout.blade.php not supporting hte new themes

--- resources/views/layouts/loggedout.blade.php
+++ resources/views/layouts/loggedout.blade.php
@@ -18,6 +18,13 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @auth
     @switch (Auth::user()->settings->theme)
+    @case(5)
+    <link href="{{ asset('css/app.css?id=' . Str::random(8)) }}" rel="stylesheet">
+    <link href="{{ asset('css/appdark.css?id=' . Str::random(8)) }}" rel="stylesheet">
+    @break
+    @case(4)
+    <link href="{{ asset('css/app.css?id=' . Str::random(8)) }}" rel="stylesheet">
+    @break
     @case(3)
     <link href="{{ asset('css/app.css?id=' . Str::random(8)) }}" rel="stylesheet">
     <link href="{{ asset('css/appdark.css?id=' . Str::random(8)) }}" rel="stylesheet">
@@ -26,7 +33,6 @@
     <link href="{{ asset('css/app.css?id=' . Str::random(8)) }}" rel="stylesheet">
     <link href="{{ asset('css/2018.css?id=' . Str::random(8)) }}" rel="stylesheet">
     @break
-
     @default
     <link href="{{ asset('css/app.css?id=' . Str::random(8)) }}" rel="stylesheet">
     @endswitch
