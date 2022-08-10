9cf5d376226655c5f985fa8b56f9f9ee25fb504b
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-11 03:49:49 +1000

    corescript fix

--- public/asset/index.php
+++ public/asset/index.php
@@ -1,5 +1,10 @@
 <?php
 ob_start();
+function sign($data) {
+    $PrivKey = file_get_contents("./PrivKey.pem");
+    openssl_sign($data, $signature, $PrivKey, OPENSSL_ALGO_SHA1);
+    echo sprintf("%%%s%%%s", base64_encode($signature), $data);
+}
 ini_set('user_agent', 'Roblox/WinInet'); // required for some audios
 
 $id = (int)($_GET["id"] ?? die(json_encode(["message" => "Error handling your request."])));
@@ -8,6 +13,51 @@ $version = isset($_GET["version"]) ? filter_var($_GET["version"], FILTER_SANITIZ
 
 if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/asset/" . $id)){
     $file = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/asset/" . $id);
+    if (isset($_GET["sign"]) == true){
+        sign($file);
+	}else if($id == 1){
+        sign($file);
+	}else if($id == 2){
+		sign($file);
+	}else if($id == 3){
+		sign($file);
+	}else if($id == 4){
+		sign($file);
+	}else if($id == 5){
+		sign($file);
+	}else if($id == 7){
+		sign($file);
+	}else if($id == 6){
+		sign($file);
+	}else if($id == 8){
+        sign($file);
+	}else if($id == 9){
+		sign($file);
+	}else if($id == 10){
+		sign($file);
+	}else if($id == 11){
+		sign($file);
+	}else if($id == 12){
+		sign($file);
+	}else if($id == 13){
+		sign($file);
+	}else if($id == 14){
+        sign($file);
+	}else if($id == 15){
+		sign($file);
+	}else if($id == 16){
+		sign($file);
+	}else if($id == 17){
+		sign($file);
+	}else if($id == 18){
+		sign($file);
+	}else if($id == 19){
+		sign($file);
+	}else if($id == 20){
+        sign($file);
+    }else{
+        echo $file;
+    }
 }else{
     header("Location: https://assetdelivery.roblox.com/v1/asset/?id=$id&version=$version");
     die();
