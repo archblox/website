522701ad38b729bed7ace576cb381c13865c3c92
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-17 17:02:31 +1000

    Update edit.blade.php

--- resources/views/client/edit.blade.php
+++ resources/views/client/edit.blade.php
@@ -1,9 +1,8 @@
 @php
-header("content-type:text/plain");
 ob_start();
 $PlaceID = addslashes($_GET["PlaceID"]);
 @endphp
-game:Load("http://morblox.us/asset?id=$PlaceID")
+game:Load("http://morblox.us/asset?id=@php echo $PlaceID @endphp")
 @php
 $data = ob_get_clean();
 $signature;
