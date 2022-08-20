86271f7920e01daaebea983168dab99e2ca906ad
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-20 10:17:25 +1000

    Update idelanding.blade.php

--- resources/views/client/idelanding.blade.php
+++ resources/views/client/idelanding.blade.php
@@ -6,10 +6,10 @@
 <html>
 <head>
     <title>Welcome</title>
-    <script type="text/javascript" src="https://web.archive.orgweb.archive.org/web/20130702194455js_/http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js"></script>
-<script type="text/javascript">window.jQuery || document.write("<script type='text/javascript' src='/js/jquery/jquery-1.7.2.min.js'><\/script>")</script>
-<script type="text/javascript" src="https://web.archive.orgweb.archive.org/web/20130702194455js_/http://ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js"></script>
-<script type="text/javascript">window.Sys || document.write("<script type='text/javascript' src='/js/Microsoft/MicrosoftAjax.js'><\/script>")</script>
+    <script type="text/javascript" src="https://web.archive.org/web/20130702194455js_/http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js"></script>
+<script type="text/javascript">window.jQuery || document.write("<script type='text/javascript' src='https://web.archive.org/web/20130702194455js_/http://roblox.com//js/jquery/jquery-1.7.2.min.js'><\/script>")</script>
+<script type="text/javascript" src="https://web.archive.org/web/20130702194455js_/http://ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js"></script>
+<script type="text/javascript">window.Sys || document.write("<script type='text/javascript' src='https://web.archive.org/web/20130702194455js_/http://roblox.com/js/Microsoft/MicrosoftAjax.js'><\/script>")</script>
 
     
 <link rel="stylesheet" href="https://web.archive.org/web/20130702194455cs_/http://roblox.com/CSS/Base/CSS/FetchCSS?path=page___70a75bfda0b97b53e5637a9e3af568ee_m.css"/>
@@ -20,7 +20,7 @@
     
     <script type="text/javascript">Roblox.config.externalResources = ['/js/jquery/jquery-1.7.2.min.js'];Roblox.config.paths['jQuery'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/29cf397a226a92ca602cb139e9aae7d7.js';Roblox.config.paths['Pagelets.BestFriends'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/c8acaba4214074ed4ad6f8b4a9647038.js';Roblox.config.paths['Pages.Catalog'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/c8f61a230e6ad34193b40758f1499a3d.js';Roblox.config.paths['Pages.Messages'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/b84022adb990981fb0bd838cda0f67d5.js';Roblox.config.paths['Resources.Messages'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/fb9cb43a34372a004b06425a1c69c9c4.js';Roblox.config.paths['Widgets.AvatarImage'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/b7f418a5fefacfd21f2c86b495b4698f.js';Roblox.config.paths['Widgets.DropdownMenu'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/d83d02dd89808934b125fa21c362bcb9.js';Roblox.config.paths['Widgets.GroupImage'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/3e692c7b60e1e28ce639184f793fdda9.js';Roblox.config.paths['Widgets.HierarchicalDropdown'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/e8b579b8e31f8e7722a5d10900191fe7.js';Roblox.config.paths['Widgets.ItemImage'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/facde7fc56e53e1ef9ee75203bc76bb4.js';Roblox.config.paths['Widgets.PlaceImage'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/08e1942c5b0ef78773b03f02bffec494.js';Roblox.config.paths['Widgets.Suggestions'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/a63d457706dfbc230cf66a9674a1ca8b.js';Roblox.config.paths['Widgets.SurveyModal'] = 'https://web.archive.org/web/20130702194455/http://jsak.roblox.com/d6e979598c460090eafb6d38231159f6.js';</script>
         <script type="text/javascript">
-            function editTemplateInStudio(play_placeId) { RobloxLaunch._GoogleAnalyticsCallback = function() { var isInsideRobloxIDE = 'website'; if (Roblox && Roblox.Client && Roblox.Client.isIDE && Roblox.Client.isIDE()) { isInsideRobloxIDE = 'Studio'; };GoogleAnalyticsEvents.FireEvent(['Edit Location', 'Guest', isInsideRobloxIDE]);GoogleAnalyticsEvents.FireEvent(['Edit', 'Guest', '']); };  Roblox.Client.WaitForRoblox(function() { RobloxLaunch.StartGame('https://web.archive.org/web/20130702194455/http://www.roblox.com//Game/edit.ashx?PlaceID='+play_placeId+'&upload=', 'edit.ashx', 'https://web.archive.org/web/20130702194455/https://www.roblox.com//Login/Negotiate.ashx', 'FETCH', true); }); }
+            function editTemplateInStudio(play_placeId) { RobloxLaunch._GoogleAnalyticsCallback = function() { var isInsideRobloxIDE = 'website'; if (Roblox && Roblox.Client && Roblox.Client.isIDE && Roblox.Client.isIDE()) { isInsideRobloxIDE = 'Studio'; };GoogleAnalyticsEvents.FireEvent(['Edit Location', 'Guest', isInsideRobloxIDE]);GoogleAnalyticsEvents.FireEvent(['Edit', 'Guest', '']); };  Roblox.Client.WaitForRoblox(function() { RobloxLaunch.StartGame('https://archblox.com/Game/edit.ashx?PlaceID='+play_placeId+'&upload=', 'edit.ashx', 'https://web.archive.org/web/20130702194455/https://www.roblox.com//Login/Negotiate.ashx', 'FETCH', true); }); }
         </script>
 </head>
 <body id="StudioWelcomeBody">
@@ -40,9 +40,6 @@
             </div>
         <!-- This is only after the login stuff because IE7 demands floated elements be before non-floated -->
         <img src="https://web.archive.org/web/20130702194455im_/http://roblox.com/images/IDE/img-studio_title.png" alt="Roblox Studio Title"/>
-        <p id="HomeLink">
-            <a class="text-link" href="https://web.archive.org/web/20130702194455/http://roblox.com/Build/Default.aspx">Switch to Classic View</a>
-        </p>
     </div>
     <div class="container">
         <div class="navbar">
@@ -77,26 +74,6 @@
                         <div class="template" placeid="201">
                             <a href="" class="game-image"><img class="" src="https://web.archive.org/web/20130702194455im_/http://t7ak.roblox.com/ab99a6e34406eb5e7aebd349c90ce35b"/></a>
                             <p>Flat Terrain</p>
-                        </div>
-                </div>
-                <div class="templates" js-data-templatetype="Strategy">
-                        <div class="template" placeid="92721754">
-                            <a href="" class="game-image"><img class="" src="https://web.archive.org/web/20130702194455im_/http://t3ak.roblox.com/2db15742ba86f0dfe6cd2762a8debbde"/></a>
-                            <p>Capture The Flag</p>
-                        </div>
-                        <div class="template" placeid="95269276">
-                            <a href="" class="game-image"><img class="" src="https://web.archive.org/web/20130702194455im_/http://t2ak.roblox.com/426308e89cd36c0bb531b9dd8e990c10"/></a>
-                            <p>Control Points</p>
-                        </div>
-                </div>
-                <div class="templates" js-data-templatetype="Action">
-                        <div class="template" placeid="92721884">
-                            <a href="" class="game-image"><img class="" src="https://web.archive.org/web/20130702194455im_/http://t7ak.roblox.com/4ae1c9ec91cd33a9b38ad741ac1d0a4f"/></a>
-                            <p>Free For All</p>
-                        </div>
-                        <div class="template" placeid="95205458">
-                            <a href="" class="game-image"><img class="" src="https://web.archive.org/web/20130702194455im_/http://t3ak.roblox.com/f6e818f03afa39b999a4ed33b464c0cb"/></a>
-                            <p>Team Deathmatch</p>
                         </div>
                 </div>
             </div>
