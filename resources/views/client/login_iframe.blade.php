408f131ef5f934ec147295268a25ede9a2530425
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-21 21:28:29 +1000

    Update login_iframe.blade.php

--- resources/views/client/login_iframe.blade.php
+++ resources/views/client/login_iframe.blade.php
@@ -5,42 +5,62 @@
         ARCHBLOX Login
     </title>
     <link rel="stylesheet"
-        href="http://web.archive.org/web/20130715023249cs_/https://www.roblox.com/CSS/Base/CSS/FetchCSS?path=main___bf363dfd925930252a5f326c7990c31c_m.css">
+        href="https://web.archive.org/web/20130715023249cs_/https://www.roblox.com/CSS/Base/CSS/FetchCSS?path=main___bf363dfd925930252a5f326c7990c31c_m.css">
 </head>
 
 <body style="background: #E1E1E1;">
-    <div id="LoginForm">
-        <div class="newLogin" id="newLoginContainer">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="UserNameDiv">
-                    <label class="LoginFormLabel" for="UserName">Username</label>
-                    <br><input name="login" type="text" id="login" value="{{ old('login') }}" class="LoginFormInput"
-                        required autofocus tabindex="1">
+    <div id="NotLoggedInPanel">
+        <form name="FacebookLoginForm" method="post" id="FacebookLoginForm">
+            <div id="LoginForm">
+                <div class="newLogin" id="newLoginContainer">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="UserNameDiv">
+                            <label class="LoginFormLabel" for="UserName">Username</label>
+                            <br><input name="login" type="text" id="login" value="{{ old('login') }}"
+                                class="LoginFormInput" required autofocus tabindex="1">
+                        </div>
+                        <div class="PasswordDiv">
+                            <label class="LoginFormLabel" for="Password">Password</label>
+                            <a href="ResetPasswordRequest.aspx" target="_top" class="ResetPassword">Forgot password?</a>
+                            <br><input name="password" type="password" id="password" class="LoginFormInput" required
+                                autocomplete="current-password" style="width:152px;" tabindex="2">
+                        </div>
+                        <div id="ErrorMessage" style="color:Red"></div>
+                        <div class="LoginFormFieldSet">
+                            <span
+                                style="font: normal 12px arial;color: black;position: absolute;top: 50%;margin-top: -8px;">
+                                Not a member?
+                                <a href="{{ route('register') }}" style="font: bold 12px Arial;color: #095FB5;"
+                                    target="_top">Sign up!</a>
+                            </span>
+                            <span id="LoginButtonActive"><a id="LoginButton" type="submit"
+                                    onClick="this.form.submit();this.disabled=true" tabindex="4"
+                                    class="iFrameBlueLogin"></a></span>
+                            <span id="LoggingInStatus"
+                                style="display: none; font: bold 12px arial; position: absolute; right: 8px;margin-top: -11px;top: 50%;">
+                                <img src="https://web.archive.org/web/20130715023249im_/https://s3.amazonaws.com/images.roblox.com/6ec6fa292c1dcdb130dcf316ac050719.gif"
+                                    style="margin-right: 5px;width: 20px;height: 20px;" alt="">
+                                <span style="top: -5px;position: absolute;position: relative;">Logging in...</span>
+                            </span>
+                        </div>
+                    </form>
+                    <div id="SocialNetworkSignIn">
+                        <div id="fb-root"></div>
+
+                        <div id="facebookSignIn">
+                            <a class="facebook-login"
+                                href="/web/20130715023249/https://www.roblox.com/Facebook/SignIn?returnTo=http%3a%2f%2fwww.roblox.com%2fIDE%2fwelcome"
+                                target="_top" ref="form-facebook">
+                                <span class="left"></span>
+                                <span class="middle">Login with Facebook<span>Login with Facebook</span></span>
+                                <span class="right"></span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-                <div class="PasswordDiv">
-                    <label class="LoginFormLabel" for="Password">Password</label>
-                    <a href="ResetPasswordRequest.aspx" target="_top" class="ResetPassword">Forgot password?</a>
-                    <br><input name="password" type="password" id="password" class="LoginFormInput" required
-                        autocomplete="current-password" style="width:152px;" tabindex="2">
-                </div>
-            <div id="ErrorMessage" style="color:Red"></div>
-            <div class="LoginFormFieldSet">
-                <span style="font: normal 12px arial;color: black;position: absolute;top: 50%;margin-top: -8px;">
-                    Not a member?
-                    <a href="NewAge.aspx" style="font: bold 12px Arial;color: #095FB5;" target="_top">Sign up!</a>
-                </span>
-                <span id="LoginButtonActive"><a id="LoginButton" type="submit" onClick="this.form.submit();this.disabled=true" tabindex="4" class="iFrameBlueLogin"></a></span>
-                <span id="LoggingInStatus"
-                    style="display: none; font: bold 12px arial; position: absolute; right: 8px;margin-top: -11px;top: 50%;">
-                    <img src="https://web.archive.org/web/20130715023249im_/https://s3.amazonaws.com/images.roblox.com/6ec6fa292c1dcdb130dcf316ac050719.gif"
-                        style="margin-right: 5px;width: 20px;height: 20px;" alt="">
-                    <span style="top: -5px;position: absolute;position: relative;">Logging in...</span>
-                </span>
             </div>
-            </form>
-        </div>
-    </div>
+        </form>
     </div>
     <script type="text/javascript">
     $(function() {
