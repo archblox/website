39af138bc2b3f3b7f10a93c8fb641f30c532346f
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-21 20:25:27 +1000

    Update idelanding.blade.php

--- resources/views/client/idelanding.blade.php
+++ resources/views/client/idelanding.blade.php
@@ -5,6 +5,10 @@
 <head>
     <title>Welcome</title>
     <link rel="stylesheet" href="https://web.archive.org/web/20130702194455cs_/http://roblox.com/CSS/Base/CSS/FetchCSS?path=page___70a75bfda0b97b53e5637a9e3af568ee_m.css" />
+    <script type="text/javascript">
+        function editTemplateInStudio(play_placeId) { RobloxLaunch._GoogleAnalyticsCallback = function() { var isInsideRobloxIDE = 'website'; if (Roblox && Roblox.Client && Roblox.Client.isIDE && Roblox.Client.isIDE()) { isInsideRobloxIDE = 'Studio'; };GoogleAnalyticsEvents.FireEvent(['Edit Location', 'Guest', isInsideRobloxIDE]);GoogleAnalyticsEvents.FireEvent(['Edit', 'Guest', '']); };  Roblox.Client.WaitForRoblox(function() { RobloxLaunch.StartGame('https://web.archive.org/web/20130702194455/http://www.roblox.com//Game/edit.ashx?PlaceID='+play_placeId+'&upload=', 'edit.ashx', 'https://web.archive.org/web/20130702194455/https://www.roblox.com//Login/Negotiate.ashx', 'FETCH', true); }); }
+    </script>
+    <script type="text/javascript" src="{{ asset('js/test.js') }}"></script>
 </head>
 
 <body id="StudioWelcomeBody">
