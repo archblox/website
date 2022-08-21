d27061173dad4f305300abec6b4e74a1eff07307
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-21 21:30:31 +1000

    Update login_iframe.blade.php

--- resources/views/client/login_iframe.blade.php
+++ resources/views/client/login_iframe.blade.php
@@ -10,41 +10,39 @@
 
 <body style="background: #E1E1E1;">
     <div id="NotLoggedInPanel">
-        <form name="FacebookLoginForm" method="post" id="FacebookLoginForm">
+        <form name="FacebookLoginForm" id="FacebookLoginForm" method="POST" action="{{ route('login') }}">
             <div id="LoginForm">
                 <div class="newLogin" id="newLoginContainer">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="UserNameDiv">
-                            <label class="LoginFormLabel" for="UserName">Username</label>
-                            <br><input name="login" type="text" id="login" value="{{ old('login') }}"
-                                class="LoginFormInput" required autofocus tabindex="1">
-                        </div>
-                        <div class="PasswordDiv">
-                            <label class="LoginFormLabel" for="Password">Password</label>
-                            <a href="ResetPasswordRequest.aspx" target="_top" class="ResetPassword">Forgot password?</a>
-                            <br><input name="password" type="password" id="password" class="LoginFormInput" required
-                                autocomplete="current-password" style="width:152px;" tabindex="2">
-                        </div>
-                        <div id="ErrorMessage" style="color:Red"></div>
-                        <div class="LoginFormFieldSet">
-                            <span
-                                style="font: normal 12px arial;color: black;position: absolute;top: 50%;margin-top: -8px;">
-                                Not a member?
-                                <a href="{{ route('register') }}" style="font: bold 12px Arial;color: #095FB5;"
-                                    target="_top">Sign up!</a>
-                            </span>
-                            <span id="LoginButtonActive"><a id="LoginButton" type="submit"
-                                    onClick="this.form.submit();this.disabled=true" tabindex="4"
-                                    class="iFrameBlueLogin"></a></span>
-                            <span id="LoggingInStatus"
-                                style="display: none; font: bold 12px arial; position: absolute; right: 8px;margin-top: -11px;top: 50%;">
-                                <img src="https://web.archive.org/web/20130715023249im_/https://s3.amazonaws.com/images.roblox.com/6ec6fa292c1dcdb130dcf316ac050719.gif"
-                                    style="margin-right: 5px;width: 20px;height: 20px;" alt="">
-                                <span style="top: -5px;position: absolute;position: relative;">Logging in...</span>
-                            </span>
-                        </div>
-                    </form>
+                    @csrf
+                    <div class="UserNameDiv">
+                        <label class="LoginFormLabel" for="UserName">Username</label>
+                        <br><input name="login" type="text" id="login" value="{{ old('login') }}" class="LoginFormInput"
+                            required autofocus tabindex="1">
+                    </div>
+                    <div class="PasswordDiv">
+                        <label class="LoginFormLabel" for="Password">Password</label>
+                        <a href="ResetPasswordRequest.aspx" target="_top" class="ResetPassword">Forgot password?</a>
+                        <br><input name="password" type="password" id="password" class="LoginFormInput" required
+                            autocomplete="current-password" style="width:152px;" tabindex="2">
+                    </div>
+                    <div id="ErrorMessage" style="color:Red"></div>
+                    <div class="LoginFormFieldSet">
+                        <span
+                            style="font: normal 12px arial;color: black;position: absolute;top: 50%;margin-top: -8px;">
+                            Not a member?
+                            <a href="{{ route('register') }}" style="font: bold 12px Arial;color: #095FB5;"
+                                target="_top">Sign up!</a>
+                        </span>
+                        <span id="LoginButtonActive"><a id="LoginButton" type="submit"
+                                onClick="this.form.submit();this.disabled=true" tabindex="4"
+                                class="iFrameBlueLogin"></a></span>
+                        <span id="LoggingInStatus"
+                            style="display: none; font: bold 12px arial; position: absolute; right: 8px;margin-top: -11px;top: 50%;">
+                            <img src="https://web.archive.org/web/20130715023249im_/https://s3.amazonaws.com/images.roblox.com/6ec6fa292c1dcdb130dcf316ac050719.gif"
+                                style="margin-right: 5px;width: 20px;height: 20px;" alt="">
+                            <span style="top: -5px;position: absolute;position: relative;">Logging in...</span>
+                        </span>
+                    </div>
                     <div id="SocialNetworkSignIn">
                         <div id="fb-root"></div>
 
