9ad2fd9f7ba781499388ffcebc01307dc8deb92e
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-27 12:27:32 +1000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,7 +174,7 @@
         <div class="popup">
             <h2 id="heading">Launching ARCHBLOX Studio...</h2>
             <p>Don't have Archblox Studio installed?</p>
-            <button class="greenbutton" href="{{ route('download') }}">Click here to download ARCHBLOX Studio</button>
+            <a href="{{ route('download') }}"><button style="height=40px;" class="greenbutton">Click here to download ARCHBLOX Studio</button></a>
             <br>
             <button class="redbutton" onclick="closeStudioPopup()">Close</button>
         </div>
