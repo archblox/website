1666f16f77b2276e23bf98762c9d2b1beda4a6d3
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-28 12:26:28 +1000

    Update gamepage.blade.php

--- resources/views/pages/gamepage.blade.php
+++ resources/views/pages/gamepage.blade.php
@@ -22,7 +22,7 @@
         <h1>Natural Disaster Survival</h1> 
         <p>By <a href="{{ route('profile', 1) }}">ARCHBLOX</a></p>
         <p style="text-align: center;">
-            <button onclick="openClientPopup('localhost','53640','temp.rbxl','hostandplay')" class="greenbutton" style="width: 70%; height: 50px; font-size: x-large; margin-top: 115px; align-content: center;">Host &amp; Play</button>
+            <button onclick="openClientPopup('localhost','53640','temp.rbxl','hostandjoin')" class="greenbutton" style="width: 70%; height: 50px; font-size: x-large; margin-top: 115px; align-content: center;">Host &amp; Play</button>
             <button onclick="openClientPopup(null,'53640','temp.rbxl','host')" class="greenbutton" style="width: 27%;height: 50px;margin-top: 0px;font-size: x-large;align-content: center;">Host</button>
         </p>
     </div>
