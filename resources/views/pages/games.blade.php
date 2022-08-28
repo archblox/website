a087629549d1afed8bf356b7f214616db74abb4d
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-08-28 11:43:17 +1000

    fix

--- resources/views/pages/games.blade.php
+++ resources/views/pages/games.blade.php
@@ -10,7 +10,7 @@
 @endsection
 
 @section('alert')
-<div id="alert">Note that this page does not support joining yet. If you would like to join your own game that you are currently hosting, <a onclick="openClientPopup('localhost','53640',null,'join')">click here</a>.</div>
+    <div id="alert">Note that this page does not support joining yet. If you would like to join your own game that you are currently hosting, <a onclick="openClientPopup('localhost','53640',null,'join')">click here</a>.</div>
 @endsection
 
 @section('content')
@@ -18,8 +18,8 @@
 <br>
 <div class="content_special" style="flex-wrap: wrap; justify-content: space-around;">
     <div>
-        <a href="{{ route('gamespage')}}"><img class="gamethumbnail_small"></a>
-        <a href="{{ route('gamespage')}}">Natural Disaster Survival</a> 
+        <a href="{{ route('gamepage')}}"><img class="gamethumbnail_small"></a>
+        <a href="{{ route('gamepage')}}">Natural Disaster Survival</a> 
         <p>By <a href="{{ route('profile', 1) }}">ARCHBLOX</a></p>
     </div>
 </div>
