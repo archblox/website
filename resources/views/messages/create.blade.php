f50ea6045b76f86b18423bfc3f409449ce75c62c
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-11 23:19:10 +1000

    minor fixes

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -16,24 +16,22 @@
         @csrf
         @if (request()->has('replyTo'))
             <p>To: <input type="text" name="name" placeholder="Username" value="{{ $replyName }}"></p>
-            <p>Subject: <input type="text" name="subject" placeholder="Subject" value="{{ $replySubject }}"></p>
-            <button class="greybutton" type="button" onClick="document.getElementById('message').value = ''" style="margin-top:3px;margin-bottom:3px">Clear Message</button>
+            <p>Subject: <input type="text" name="subject" placeholder=" " value="{{ $replySubject }}"></p>
             <textarea style="width: calc(100% - 5px); height: 170px; min-height: 170px; resize: vertical;"
                 placeholder="Write your message..." id="message" name="message">{{ old('message') ? old('message') : $replyContent }}</textarea>
+            <button class="greybutton" type="button" onClick="document.getElementById('message').value = ''" style="margin-top:3px;margin-bottom:3px">Clear Message Text</button>
+            <br>
             <button class="greenbutton" type="submit">Send</button>
             <button class="redbutton" type="reset"><a href="{{ route('inbox') }}"
                     style="color:white;font-weight:normal">Cancel</a></button>
         @else
-            <p>To: <input type="text" name="name" placeholder="Username"
-                    @if (request()->has('to')) value="{{ request()->to }}" @else value="{{ old('name') }}" @endif>
-            </p>
+            <p>To: <input type="text" name="name" placeholder="Username" @if (request()->has('to')) value="{{ request()->to }}" @else value="{{ old('name') }}" @endif></p>
             <p>Subject: <input type="text" name="subject" placeholder="Subject" value="{{ old('subject') }}"></p>
             <button class="greybutton" type="button" onClick="document.getElementById('message').value = ''" style="margin-top:3px;margin-bottom:3px">Clear Message</button>
             <textarea style="width: 99%; width: calc(100% - 5px); height: 170px; min-height: 170px; resize: vertical;"
                 placeholder="Write your message..." id="message" name="message">{{ old('message') }}</textarea>
             <button class="greenbutton" type="submit">Send</button>
-            <button class="redbutton" type="reset"><a href="{{ route('inbox') }}"
-                    style="color:white;font-weight:normal">Cancel</a></button>
+            <button class="redbutton" type="reset"><a href="{{ route('inbox') }}" style="color:white;font-weight:normal">Cancel</a></button>
         @endif
     </form>
 @endsection
--- resources/views/pages/incomplete.blade.php
+++ resources/views/pages/incomplete.blade.php
@@ -11,7 +11,5 @@
 
     @section('content')
         <h1 id="usernameframe">Incomplete Page</h1>
-        <br>
         <p>This page is not done yet. This page will be made soon, so please be patient.</p>
-        <p>Looking for the download links for the client/studio? <a href="{{ route('download') }}">Click here</a>.</p>
     @endsection
