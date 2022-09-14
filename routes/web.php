dd23c7a9e3f97d7439886f3d6ee4016fb8779e8d
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-14 20:02:44 +1000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -99,5 +99,5 @@ Route::get('/Game/Tools/InsertAsset.ashx', [App\Http\Controllers\ClientControlle
 Route::get('/UI/Save.aspx', [App\Http\Controllers\ClientController::class, 'modelupload'])->name('modelupload');
 Route::get('/Game/Tools/ThumbnailAsset.ashx', [App\Http\Controllers\ClientController::class, 'stampertools'])->name('stampertools');
 Route::get('/Game/edit.ashx', [App\Http\Controllers\ClientController::class, 'edit'])->name('edit');
-Route::get('/Asset/BodyColors.ashx', [App\Http\Controllers\ClientController::class, 'bodycolors'])->name('bodycolors');
-Route::get('/Asset/CharacterFetch.ashx', [App\Http\Controllers\ClientController::class, 'characterfetch'])->name('characterfetch');
+//Route::get('/Asset/BodyColors.ashx', [App\Http\Controllers\ClientController::class, 'bodycolors'])->name('bodycolors');
+//Route::get('/Asset/CharacterFetch.ashx', [App\Http\Controllers\ClientController::class, 'characterfetch'])->name('characterfetch');
