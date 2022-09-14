69d429dbd29ed8ebd767a9f2ed799a2e7b094f73
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-14 20:00:38 +1000

    Update CharacterFetch.blade.php

--- resources/views/client/CharacterFetch.blade.php
+++ resources/views/client/CharacterFetch.blade.php
@@ -3,5 +3,5 @@ header("content-type:application/xml");
 ob_start();
 $userId = addslashes($_GET["userId"]);
 //TODO: add catalog + avatar system
+// http://morblox.us/Asset/BodyColors.ashx?userId=@php echo $userId @endphp;
 @endphp
-http://morblox.us/Asset/BodyColors.ashx?userId=@php echo $userId @endphp;
