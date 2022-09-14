ffe4140c15c126cc4b2b75997333d96c19f52a8d
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-14 20:01:30 +1000

    Update BodyColors.blade.php

--- resources/views/client/BodyColors.blade.php
+++ resources/views/client/BodyColors.blade.php
@@ -3,20 +3,3 @@ header("content-type:text/plain");
 $userId = addslashes($_GET["userId"]);
 //TODO: add avatar system 
 @endphp
-<?xml version="1.0" encoding="utf-8" ?>
-<roblox xmlns:xmime="http://www.w3.org/2005/05/xmlmime" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="http://www.roblox.com/roblox.xsd" version="4">
-    <External>null</External>
-    <External>nil</External>
-    <Item class="BodyColors">
-        <Properties>
-            <int name="HeadColor">45</int>
-            <int name="LeftArmColor">45</int>
-            <int name="LeftLegColor">1003</int>
-            <string name="Name">Body Colors</string>
-            <int name="RightArmColor">45</int>
-            <int name="RightLegColor">1003</int>
-            <int name="TorsoColor">1003</int>
-            <bool name="archivable">true</bool>
-        </Properties>
-    </Item>
-</roblox>
