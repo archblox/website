2363afdb11709035ee6820306ef5b62436abe32e
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-15 04:35:18 +1000

    face fix

--- resources/views/client/CharacterFetch.blade.php
+++ resources/views/client/CharacterFetch.blade.php
@@ -8,7 +8,7 @@ $hat3 = 0;
 $shirt = 306;
 $pants = 305;
 $shirtgraphic = 303;
-$face = 307;
+$face = 308;
 //TODO: add catalog + avatar system
 @endphp
 http://morblox.us/Asset/BodyColors.ashx?userId=@php echo $userId @endphp;http://morblox.us/asset/?id=@php echo $hat1 @endphp;http://morblox.us/asset/?id=@php echo $hat2 @endphp;http://morblox.us/asset/?id=@php echo $hat3 @endphp;http://morblox.us/asset/?id=@php echo $shirt @endphp;http://morblox.us/asset/?id=@php echo $pants @endphp;http://morblox.us/asset/?id=@php echo $shirtgraphic @endphp;http://morblox.us/asset/?id=@php echo $face @endphp
