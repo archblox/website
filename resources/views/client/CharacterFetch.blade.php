6246f0ab8e83f7096fba89fb89f0aee2be4ac0d1
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-15 04:18:23 +1000

    test

--- resources/views/client/CharacterFetch.blade.php
+++ resources/views/client/CharacterFetch.blade.php
@@ -2,6 +2,12 @@
 header("content-type:application/xml");
 ob_start();
 $userId = addslashes($_GET["userId"]);
+$hat1 = 300;
+$hat2 = 0;
+$hat3 = 0;
+$shirt = 306;
+$pants = 305;
+$shirtgraphic = 303;
 //TODO: add catalog + avatar system
 @endphp
-http://morblox.us/Asset/BodyColors.ashx?userId=@php echo $userId @endphp;http://morblox.us/asset/?id=300
+http://morblox.us/Asset/BodyColors.ashx?userId=@php echo $userId @endphp;http://morblox.us/asset/?id=@php echo $hat1 @endphp;http://morblox.us/asset/?id=@php echo $hat2 @endphp;http://morblox.us/asset/?id=@php echo $hat3 @endphp;http://morblox.us/asset/?id=@php echo $shirt @endphp;http://morblox.us/asset/?id=@php echo $pants @endphp;http://morblox.us/asset/?id=@php echo $shirtgraphic @endphp
