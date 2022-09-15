9fc84827d11d982b504495d6b899f2d314fa5005
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-16 08:29:45 +1000

    Update studio.blade.php

--- resources/views/client/studio.blade.php
+++ resources/views/client/studio.blade.php
@@ -6,6 +6,17 @@ ob_start();
 @endphp
 
 game:GetService("ScriptContext"):AddStarterScript(1)
+
+pcall(function() game:GetService("InsertService"):SetFreeModelUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?type=fm&q=%s&pg=%d&rs=%d") end)
+pcall(function() game:GetService("InsertService"):SetFreeDecalUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?type=fd&q=%s&pg=%d&rs=%d") end)
+
+game:GetService("ScriptInformationProvider"):SetAssetUrl("http://www.morblox.us/Asset/")
+game:GetService("InsertService"):SetBaseSetsUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?nsets=10&type=base")
+game:GetService("InsertService"):SetUserSetsUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=%d")
+game:GetService("InsertService"):SetCollectionUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?sid=%d")
+game:GetService("InsertService"):SetAssetUrl("http://www.morblox.us/Asset/?id=%d")
+game:GetService("InsertService"):SetAssetVersionUrl("http://www.morblox.us/Asset/?assetversionid=%d")
+
 @php
 $data = ob_get_clean();
 $signature;
