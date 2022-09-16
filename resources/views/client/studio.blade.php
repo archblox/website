5ad930c197a1393e87f6a20a1a9ae1f631c83566
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-09-17 08:42:57 +1000

    Update studio.blade.php

--- resources/views/client/studio.blade.php
+++ resources/views/client/studio.blade.php
@@ -7,13 +7,13 @@ ob_start();
 
 game:GetService("ScriptContext"):AddStarterScript(1)
 
-pcall(function() game:GetService("InsertService"):SetFreeModelUrl("http://www.morblox.us/game/Tools/InsertAsset.ashx?type=fm&q=%s&pg=%d&rs=%d") end)
-pcall(function() game:GetService("InsertService"):SetFreeDecalUrl("http://www.morblox.us/game/Tools/InsertAsset.ashx?type=fd&q=%s&pg=%d&rs=%d") end)
+pcall(function() game:GetService("InsertService"):SetFreeModelUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?type=fm&q=%s&pg=%d&rs=%d") end)
+pcall(function() game:GetService("InsertService"):SetFreeDecalUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?type=fd&q=%s&pg=%d&rs=%d") end)
 
 game:GetService("ScriptInformationProvider"):SetAssetUrl("http://www.morblox.us/asset/")
-game:GetService("InsertService"):SetBaseSetsUrl("http://www.morblox.us/game/Tools/InsertAsset.ashx?nsets=10&type=base")
-game:GetService("InsertService"):SetUserSetsUrl("http://www.morblox.us/game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=%d")
-game:GetService("InsertService"):SetCollectionUrl("http://www.morblox.us/game/Tools/InsertAsset.ashx?sid=%d")
+game:GetService("InsertService"):SetBaseSetsUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?nsets=10&type=base")
+game:GetService("InsertService"):SetUserSetsUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?nsets=20&type=user&userid=%d")
+game:GetService("InsertService"):SetCollectionUrl("http://www.morblox.us/Game/Tools/InsertAsset.ashx?sid=%d")
 game:GetService("InsertService"):SetAssetUrl("http://www.morblox.us/asset/?id=%d")
 game:GetService("InsertService"):SetAssetVersionUrl("http://www.morblox.us/asset/?assetversionid=%d")
 
