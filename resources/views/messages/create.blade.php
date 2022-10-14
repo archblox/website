62f1a7fcc368fbe297151b0fe51d5ba6cf66ac19
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-10-15 09:28:53 +1100

    Update create.blade.php

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -10,7 +10,13 @@
 @endsection
 
 @section('content')
+    <div class="PageTitleBar">
     <h1 id="usernameframe">New Message</h1>
+    <div>
+    <button class="greybutton" type="button" onClick="document.getElementById('message').value = ''" style="margin-top:3px;margin-bottom:3px">Clear Message</button>
+    <button class="redbutton" type="reset"><a href="{{ route('inbox') }}" style="font-weight:normal">Cancel</a></button>
+    </div>
+    </div>
     <br>
     <form action="{{ route('send_message') }}" method="POST">
         @csrf
@@ -21,16 +27,12 @@
                 placeholder="Write your message..." id="message" name="message">{{ old('message') ? old('message') : $replyContent }}</textarea>
             <button class="greybutton" type="button" onClick="document.getElementById('message').value = ''" style="margin-top:3px;margin-bottom:3px">Clear Message</button>
             <button class="greenbutton" type="submit">Send</button>
-            <button class="redbutton" type="reset"><a href="{{ route('inbox') }}"
-                    style="color:white;font-weight:normal">Cancel</a></button>
         @else
             <p>To: <input style="width: 95%" type="text" name="name" placeholder="Enter a username..." @if (request()->has('to')) value="{{ request()->to }}" @else value="{{ old('name') }}" @endif></p>
             <p>Subject: <input style="width: 91%" type="text" name="subject" placeholder=" " value="{{ old('subject') }}"></p>
             <textarea style="width: 99%; width: calc(100% - 5px); height: 170px; min-height: 170px; resize: vertical;"
             placeholder="Write your message..." id="message" name="message">{{ old('message') }}</textarea>
-            <button class="greybutton" type="button" onClick="document.getElementById('message').value = ''" style="margin-top:3px;margin-bottom:3px">Clear Message</button>
             <button class="greenbutton" type="submit">Send</button>
-            <button class="redbutton" type="reset"><a href="{{ route('inbox') }}" style="color:white;font-weight:normal">Cancel</a></button>
         @endif
     </form>
 @endsection
