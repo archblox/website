f23fdc21e9c65d280f7cc5ef72eb49b9d6feaf89
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2022-10-30 21:18:18 +1100

    Update detect.js

--- public/js/detect.js
+++ public/js/detect.js
@@ -10,3 +10,12 @@ function addWarning() {
 if (window.document.location.hostname == "archblox.com") {} else {
     addWarning();
 }
+
+console.log("Woah there!");
+console.log(" ");
+console.log("Be careful what you type or copy in this here javascript console.");
+console.log("Don't copy and paste some javascript code from a random person on the internet.");
+console.log("It can be an account token stealer, which can get that random person into your account.");
+console.log("Be safe, and be smart.");
+console.log(" ");
+console.log("If you know what you're doing, have fun then!");
