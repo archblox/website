4254bc78ee0ebe4c0f6322cc2b9cf1542af8ff37
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2023-03-06 19:16:12 +1100

    update

--- resources/views/pages/download.blade.php
+++ resources/views/pages/download.blade.php
@@ -22,7 +22,7 @@
 <p><a href="{{ asset('launcher/ARCHBLOXLauncher-32bit.exe') }}">32 Bit</a> | <a
         href="{{ asset('launcher/ARCHBLOXLauncher-64bit.exe') }}">64 Bit</a></p>
 <br>
-<p>ARCHBLOX Studio Bootstrapper v1.5</p>
+<p>ARCHBLOX Studio Bootstrapper v1.6</p>
 <p><a href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-32bit.exe') }}">32 Bit</a> | <a
         href="{{ asset('bootstrapper/ARCHBLOXBootstrapper-64bit.exe') }}">64 Bit</a></p>
 <br>
