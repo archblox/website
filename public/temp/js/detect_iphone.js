225e7ce7630117c2ac1096c0aaf6429372e4f679
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2023-03-07 10:37:24 +1100

    introducing: the j

---
+++ public/temp/js/detect_iphone.js
@@ -0,0 +1,8 @@
+// warning
+console.log("Stop, admin!");
+console.log(" ");
+console.log("Be cautious of what you entering/pasting in here, as it can be an account token stealer, which can get that person access to your account and the entire admin panel.");
+console.log("Be safe, and be smart.");
+console.log(" ");
+console.log("If you know what you're doing, go ahead!");
+console.log(" ");
