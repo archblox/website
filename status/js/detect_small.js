f6b67686c1f8d06512830e99554fc26c06b04db1
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2023-03-31 11:01:10 +1100

    remove /status/

--- status/js/detect_small.js
+++
@@ -1,8 +0,0 @@
-// warning
-console.log("Stop!");
-console.log(" ");
-console.log("Be cautious of what you entering/pasting in here, as it can be an account token stealer, which can get that person access to your account.");
-console.log("Be safe, and be smart.");
-console.log(" ");
-console.log("If you know what you're doing, have fun then!");
-console.log(" ");
