de6e1deb9ad02a7bd0d3e3c4769186097ba0509f
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2023-04-21 06:12:38 +1000

    Add new font, custom audio player,

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,8 +1,8 @@
 function getDarkMode() {
     var currentTime = new Date().getHours();
-    if (6 >= currentTime || currentTime > 18) {
+    if (6 >= currentTime || currentTime > 16) {
         var li = document.createElement('link');
-        var href = "{{ asset('css/appdark.css?id='.Str::random(8)) }}";
+        var href = "https://archblox.com/css/appdark.css";
         var rel = 'stylesheet';
         li.setAttribute('href', href);
         li.setAttribute('rel', rel);
