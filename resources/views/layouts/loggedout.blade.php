ff96a6c48b6bf6a5426ba7fbbb336bd0d0da2dff
Thomas G <62822072+Thomasluigi07@users.noreply.github.com>
2023-04-21 07:31:13 +1000

    Add HD icons

--- resources/views/layouts/loggedout.blade.php
+++ resources/views/layouts/loggedout.blade.php
@@ -90,6 +90,8 @@ $splash = array(
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
     <link rel="apple-touch-icon" href="{{ asset('img/MORBLOX.png') }}" />
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('img/MORBLOX.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/MORBLOX.png') }}">
     <link rel="apple-touch-startup-image" href="{{ asset('img/MORBLOXsplash.png') }}" />
     <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
